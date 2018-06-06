0c2f6045271dcf1f2389e3185797b5b56ddc1a80
Ali Najafizadeh <a.najafizadeh@gmail.com>
2018-06-06 10:56:12 -0400

    make sure that close happens once in Observe

--- observe.go
+++ observe.go
@@ -1,15 +1,20 @@
 package callbag
 
+import "sync"
+
 func Observe(op func(val interface{})) Sink {
 	return func(source Source) {
 		done := make(chan struct{}, 1)
+		once := sync.Once{}
 
 		source(NewGreets(func(p Payload) {
 			switch v := p.(type) {
 			case Data:
 				op(v.Value())
 			case Terminate:
-				close(done)
+				once.Do(func() {
+					close(done)
+				})
 			}
 		}))
 
